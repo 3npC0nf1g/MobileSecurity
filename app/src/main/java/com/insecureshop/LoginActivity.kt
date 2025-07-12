85ad941faf50fd749702c4862f2d2062acc3596b
3npC0nf1g <teumayouomo113@gmail.com>
2025-07-12 22:41:56 +0200

    fix: Insecure loging

--- app/src/main/java/com/insecureshop/LoginActivity.kt
+++ app/src/main/java/com/insecureshop/LoginActivity.kt
@@ -12,7 +12,6 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.insecureshop.databinding.ActivityLoginBinding
-import com.insecureshop.util.Prefs
 import com.insecureshop.util.Util
 
 class LoginActivity : AppCompatActivity() {
@@ -21,49 +20,56 @@ class LoginActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding =
-            DataBindingUtil.setContentView<ActivityLoginBinding>(this, R.layout.activity_login)
-        when {
-            else -> {
-                requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE), 100)
-            }
-        }
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
+        requestPermissions(
+            arrayOf(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ), 100
+        )
     }
-
 
     fun onLogin(view: View) {
         val username = mBinding.edtUserName.text.toString()
         val password = mBinding.edtPassword.text.toString()
 
-        Log.d("userName", username)
-        Log.d("password", password)
+        if (BuildConfig.DEBUG) {
+            Log.d("LoginActivity", "Login attempt detected")
+        }
 
-
-        var auth = Util.verifyUserNamePassword(username, password)
+        val auth = Util.verifyUserNamePassword(username, password)
         if (auth) {
             Util.saveProductList(this)
             val intent = Intent(this, ProductListActivity::class.java)
             startActivity(intent)
         } else {
             for (info in packageManager.getInstalledPackages(0)) {
-                var packageName = info.packageName
+                val packageName = info.packageName
                 if (packageName.startsWith("com.insecureshopapp")) {
                     try {
-                        val packageContext = createPackageContext(packageName, Context.CONTEXT_INCLUDE_CODE or Context.CONTEXT_IGNORE_SECURITY)
+                        val packageContext = createPackageContext(
+                            packageName,
+                            Context.CONTEXT_INCLUDE_CODE or Context.CONTEXT_IGNORE_SECURITY
+                        )
                         val value: Any = packageContext.classLoader
                             .loadClass("com.insecureshopapp.MainInterface")
                             .getMethod("getInstance", Context::class.java)
                             .invoke(null, this)
-                        Log.d("object_value", value.toString())
+                        if (BuildConfig.DEBUG) {
+                            Log.d("LoginActivity", "Object from dynamic load: $value")
+                        }
                     } catch (e: Exception) {
                         throw RuntimeException(e)
                     }
                 }
             }
 
-            Toast.makeText(applicationContext, "Invalid username and password", Toast.LENGTH_LONG)
-                .show()
+            Toast.makeText(
+                applicationContext,
+                "Invalid username and password",
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 }
